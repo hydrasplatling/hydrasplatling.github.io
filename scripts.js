f562a1070ae9397c50aeb617ad7ce1ef091212d7
hydrasplatling <nightfire2003@gmail.com>
2022-11-16 02:33:26 +1100

    initial

---
+++ scripts.js
@@ -0,0 +1,15 @@
+
+$('#about-toggle').click(function(){
+    $(".section").not(".about").slideUp();
+    $(".about").slideToggle();
+ });
+
+ $('#onions-toggle').click(function(){
+    $(".section").not(".onions").slideUp();
+    $(".onions").slideToggle();
+ });
+
+ $('#contact-toggle').click(function(){
+    $(".section").not(".contact").slideUp();
+    $(".contact").slideToggle();
+ });
